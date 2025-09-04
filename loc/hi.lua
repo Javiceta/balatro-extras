90e36bf9725d5ccd697ac88cea39abd2533162d5
Javiceta <javigarciaiceta@gmail.com>
2025-09-04 18:07:27 +0100

    add IRL friends' names as bosses

---
+++ loc/hi.lua
@@ -0,0 +1,90 @@
+return {
+  descriptions = {
+    Blind = {
+      bl_arm = {
+        name = "Isma",
+      },
+      bl_club = {
+        name = "Miguel",
+      },
+      bl_eye = {
+        name = "María",
+      },
+      bl_final_acorn = {
+        name = "Javi",
+      },
+      bl_final_bell = {
+        name = "Airam",
+      },
+      bl_final_heart = {
+        name = "Luis",
+      },
+      bl_final_leaf = {
+        name = "Manu",
+      },
+      bl_final_vessel = {
+        name="El Otro Dani",
+      },
+      bl_fish = {
+        name = "Vicen",
+      },
+      bl_flint = {
+        name = "Noel",
+      },
+      bl_goad = {
+        name = "Ana"
+      },
+      bl_head = {
+        name = "Diego",
+      },
+      bl_hook = {
+        name = "Jana",
+      },
+      bl_house = {
+        name = "Yago",
+      },
+      bl_manacle = {
+        name = "David",
+      },
+      bl_mark = {
+        name = "Noda",
+      },
+      bl_mouth = {
+        name="Peter",
+      },
+      bl_needle = {
+        name = "Néstor",
+      },
+      bl_ox = {
+        name = "Chema",
+      },
+      bl_pillar = {
+        name = "Juanjo",
+      },
+      bl_plant = {
+        name = "Uli",
+      },
+      bl_psychic = {
+        name = "Carol",
+      },
+      bl_serpent = {
+        name = "Víctor",
+      },
+      bl_tooth = {
+        name = "Maury",
+      },
+      bl_wall = {
+        nam = "Dani",
+      },
+      bl_water = {
+        name = "Erik",
+      },
+      bl_wheel = {
+        name = "Pablo",
+      },
+      bl_window = {
+        name = "Guti",
+      },
+    },
+  }
+}
